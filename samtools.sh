5e4b0ba5b6bfe22a813845efc822d5e978a1025f
wangshikang <43208388+wangshikang@users.noreply.github.com>
2019-06-25 15:01:06 +0800

    Update samtools.sh

--- samtools.sh
+++ samtools.sh
@@ -1,5 +1,5 @@
 #samtobam
-find . -name "*sam" |cut -c 1-14 |sort > 1
+find . -name "*sam" |grep -v '\._' |cut -c 1-14 |sort > 1
 
 cat 1 |while read id
 do
@@ -18,3 +18,19 @@ nohup samtools sort K27ac-seq--5.bam -o K27ac-seq--5.sort.bam -@ 10 &
 nohup samtools sort K27ac-seq--6.bam -o K27ac-seq--6.sort.bam -@ 10 &
 nohup samtools sort K27ac-seq--7.bam -o K27ac-seq--7.sort.bam -@ 10 &
 nohup samtools sort K27ac-seq--8.bam -o K27ac-seq--8.sort.bam -@ 10 &
+
+#去除PCR重复
+find . -name "*sort.bam" |grep -v '\._' |cut -c 1-14 |sort > 1
+cat 1 |while read id
+do
+arr=(${id})
+bam=${arr[0]}
+echo $bam
+nohup samtools rmdup $bam.sort.bam $bam.rmdup.bam &
+done
+
+#建立索引
+for a in $(ls ./*rmdup.bam)
+do  
+nohup samtools index $a -@ 8 &
+done
